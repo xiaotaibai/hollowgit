4a962bee20a3e86f587643f1ecd57e914c06c765
Administrator <Administrator@xiaotaibai>
2017-09-05 22:28:23 +0800

    test1

--- src/com/test/dao/hellowgit.java
+++ src/com/test/dao/hellowgit.java
@@ -5,6 +5,7 @@ public class hellowgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hellowgit");
+		System.out.println("updata");
 	}
 
 }
